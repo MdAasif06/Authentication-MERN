ede070bc0ea1e09636397e3a19f54694f7b796e0
MdAasif06 <abdurm477@gmail.com>
2025-09-15 11:13:07 +0530

    register and login feature are created

---
+++ server/controllers/authController.js
@@ -0,0 +1,84 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+///Register user
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.json({
+      success: false,
+      message: "Missing details",
+    });
+  }
+  try {
+    const userExists = await userModel.findOne({ email });
+    if (userExists) {
+      res.json({
+        success: false,
+        message: "user already exits in database",
+      });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = new userModel({
+      name: name,
+      email: email,
+      password: hashPassword,
+    });
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    return res.json({
+      success: true,
+      message: "user register successfully",
+    });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+//login user
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Email and password are required",
+    });
+  }
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "Invalid email",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return res.json({ success: true, message: "user login successfult" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,17 +2,23 @@ import express from "express"
 import cors from "cors"
 import 'dotenv/config'
 import cookieParser from "cookie-parser"
-
+import connectDb from "./config/mongodb.js"
+import userRoute from "./routes/userRoute.js"
+connectDb()
 const app=express();
 const port=process.env.PORT || 4000;
 
+
+app.use(express.urlencoded({extends:true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials:true}));
 
 app.get('/',(req,res)=>{
-    res.send("API working")
+    res.send("API working fine")
 })
+
+app.use("/api/user",userRoute)
 
 
 app.listen(port,(req,res)=>{
