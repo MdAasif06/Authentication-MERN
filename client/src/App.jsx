0eef33334c569a80efa67cba12550aaae4d3dbcf
MdAasif06 <abdurm477@gmail.com>
2025-09-18 21:55:01 +0530

    frontend basic setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Home from "./pages/Home.jsx"
+import Login from "./pages/Login.jsx"
+import EmailVerify from "./pages/EmailVerify.jsx"
+import ResetPassword from "./pages/ResetPassword.jsx"
+import {Route,Routes} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/email-verify' element={<EmailVerify/>}/>
+      <Route path='/reset-password' element={<ResetPassword/>}/>
+      
+    </Routes>
+    </div>
+    
+  )
+}
+
+export default App
